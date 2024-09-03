7af14775030eb7e591ae0e602f79b41f970e2319
Meezzi <alswl-3016@naver.com>
2024-09-03 23:06:27 +0900

    feat: 로그인 화면 - 로그인 및 홈화면 Navigation 구현

--- app/src/main/java/com/meezzi/localtalk/MainActivity.kt
+++ app/src/main/java/com/meezzi/localtalk/MainActivity.kt
@@ -4,44 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import com.meezzi.localtalk.repository.AuthRepository
+import com.meezzi.localtalk.ui.common.SignInNavigation
+import com.meezzi.localtalk.ui.intro.IntroViewModel
 import com.meezzi.localtalk.ui.theme.LocalTalkTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val introViewModel by viewModels<IntroViewModel> {
+        IntroViewModel.provideFactory(
+            AuthRepository(this)
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             LocalTalkTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SignInNavigation(introViewModel)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LocalTalkTheme {
-        Greeting("Android")
     }
 }
