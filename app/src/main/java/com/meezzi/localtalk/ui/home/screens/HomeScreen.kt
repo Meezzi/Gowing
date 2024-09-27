883c06782d6382ea78c8df7636f18cdabbac8c12
Meezzi <alswl-3016@naver.com>
2024-09-27 12:14:13 +0900

    design: 권한 로직 수정

--- app/src/main/java/com/meezzi/localtalk/ui/home/screens/HomeScreen.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/home/screens/HomeScreen.kt
@@ -2,19 +2,16 @@ package com.meezzi.localtalk.ui.home.screens
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import com.meezzi.localtalk.permission.RequestLocationPermission
 import com.meezzi.localtalk.ui.home.HomeViewModel
 
 @Composable
 fun HomeScreen(homeViewModel: HomeViewModel) {
 
-    val locationPermissionGranted by homeViewModel.locationPermissionGranted.collectAsState()
+    CheckPermissions()
 
-    if (!locationPermissionGranted) {
-        RequestLocationPermission()
-    }
+}
 
+@Composable
+fun HomeContent() {
     Text(text = "HomeScreen")
 }
