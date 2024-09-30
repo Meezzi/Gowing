26e3cb42ef614078c7b3cffa1c9ac5b39f3dd78e
Meezzi <alswl-3016@naver.com>
2024-09-30 23:04:50 +0900

    feat: Top App Bar 레이아웃

--- app/src/main/java/com/meezzi/localtalk/ui/common/Components.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/common/Components.kt
@@ -1,7 +1,9 @@
 package com.meezzi.localtalk.ui.common
 
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 
 @Composable
@@ -9,5 +11,17 @@ fun TextTitleLarge(text: String) {
     Text(
         text = text,
         style = MaterialTheme.typography.titleLarge
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTopAppBar(
+    title: String,
+) {
+    TopAppBar(
+        title = {
+            Text(title)
+        },
     )
 }
