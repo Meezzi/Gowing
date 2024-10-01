4451dfdd519aa7ee9fd754556effc1abeeb016ca
Meezzi <alswl-3016@naver.com>
2024-10-01 10:10:50 +0900

    design: 글쓰기 화면 생성

---
+++ app/src/main/java/com/meezzi/localtalk/ui/addPost/AddPostScreen.kt
@@ -0,0 +1,9 @@
+package com.meezzi.localtalk.ui.addPost
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AddPostScreen() {
+    Text(text = "CreatePost")
+}
