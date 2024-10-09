26fad56b554ed22fd245e0dc8c95e7e4ddf52e6e
Meezzi <alswl-3016@naver.com>
2024-10-09 23:56:03 +0900

    feat: 접근 권한 거절시, 안내 Dialog 구현

--- app/src/main/java/com/meezzi/localtalk/ui/common/Components.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/common/Components.kt
@@ -1,10 +1,14 @@
 package com.meezzi.localtalk.ui.common
 
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.meezzi.localtalk.R
 
 @Composable
 fun TextTitleLarge(text: String) {
@@ -23,5 +27,23 @@ fun CustomTopAppBar(
         title = {
             Text(title)
         },
+    )
+}
+
+@Composable
+fun CustomPermissionRationaleDialog(
+    text: String,
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = onConfirm) {
+                Text(stringResource(id = R.string.confirm))
+            }
+        },
+        title = { Text(stringResource(id = R.string.permission_dialog_title)) },
+        text = { Text(text) },
     )
 }
