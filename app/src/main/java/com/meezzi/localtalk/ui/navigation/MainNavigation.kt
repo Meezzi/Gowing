8c259b80f2516e07398e4bea0b14054cb2434167
Meezzi <alswl-3016@naver.com>
2024-10-10 21:14:09 +0900

    feat: SignIn NavHost와 MainNavHost 병합

--- app/src/main/java/com/meezzi/localtalk/ui/navigation/MainNavigation.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/navigation/MainNavigation.kt
@@ -5,15 +5,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.remember
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.meezzi.localtalk.repository.HomeRepository
+import com.google.firebase.auth.FirebaseAuth
 import com.meezzi.localtalk.repository.UserRepository
 import com.meezzi.localtalk.ui.addPost.AddPostScreen
 import com.meezzi.localtalk.ui.addPost.AddPostViewModel
@@ -22,21 +23,71 @@ import com.meezzi.localtalk.ui.chat.ChatScreen
 import com.meezzi.localtalk.ui.home.HomeViewModel
 import com.meezzi.localtalk.ui.home.screens.AddPostFloatingButton
 import com.meezzi.localtalk.ui.home.screens.HomeScreen
+import com.meezzi.localtalk.ui.intro.IntroViewModel
+import com.meezzi.localtalk.ui.intro.screens.LoginScreen
+import com.meezzi.localtalk.ui.profile.CreateProfileScreen
 import com.meezzi.localtalk.ui.profile.ProfileScreen
 import com.meezzi.localtalk.ui.profile.ProfileViewModel
 
 @Composable
-fun MainNavHost(navController: NavHostController) {
+fun MainNavHost(
+    navController: NavHostController,
+    introViewModel: IntroViewModel,
+    profileViewModel: ProfileViewModel,
+    homeViewModel: HomeViewModel,
     addPostViewModel: AddPostViewModel,
+) {
+
+    val user by introViewModel.authState.collectAsStateWithLifecycle()
+
+    val startDestination = remember {
+        if (FirebaseAuth.getInstance().currentUser != null) {
+            Screen.Home.route
+        } else {
+            Screens.Login.name
+        }
+    }
+
+    LaunchedEffect(user) {
+        if (user != null) {
+            val hasData = introViewModel.hasUserData()
+            val destination = if (hasData) Screen.Home.route else Screens.CreateProfile.name
+            navController.navigate(destination)
+        }
+    }
 
     NavHost(
         navController = navController,
-        startDestination = Screen.Home.route,
+        startDestination = startDestination,
     ) {
+        composable(Screens.Login.name) {
+            LoginScreen(
+                onSignInClick = {
+                    introViewModel.signInWithGoogle()
+                },
+            )
+        }
+
+        composable(Screens.CreateProfile.name) {
+            CreateProfileScreen(
+                onProfileSaved = { nickname, profileImage ->
+                    profileViewModel.saveUserProfile(nickname, profileImage)
+                    navController.navigate(Screens.Profile.name)
+                },
+                profileViewModel = profileViewModel,
+            )
+        }
+
+        composable(Screens.Profile.name) {
+            ProfileScreen(
+                onEditProfileClick = {
+                    navController.navigate(Screens.CreateProfile.name)
+                },
+                profileViewModel = ProfileViewModel(UserRepository())
+            )
+        }
 
         composable(Screen.Home.route) {
-            val homeRepository = HomeRepository(LocalContext.current)
-            val homeViewModel = HomeViewModel(homeRepository)
             HomeScreen(
                 homeViewModel = homeViewModel
             )
