cf94f7b9abbb28713b292353e4b2ec0756fe1ff5
Meezzi <alswl-3016@naver.com>
2024-10-10 15:52:41 +0900

    feat: AddPostViewModel에 의존성 주입

--- app/src/main/java/com/meezzi/localtalk/ui/addPost/AddPostViewModel.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/addPost/AddPostViewModel.kt
@@ -4,10 +4,11 @@ import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.meezzi.localtalk.repository.PostSaveRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
-class AddPostViewModel : ViewModel() {
+class AddPostViewModel(private val postSaveRepository: PostSaveRepository) : ViewModel() {
 
     private val _title = MutableStateFlow("")
     val title: StateFlow<String> = _title
