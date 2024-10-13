d3605409ac4023713ec2423aefd837a084eb4fed
Meezzi <alswl-3016@naver.com>
2024-10-13 17:35:41 +0900

    feat: 게시물을 Firestore에 저장

--- app/src/main/java/com/meezzi/localtalk/repository/PostSaveRepository.kt
+++ app/src/main/java/com/meezzi/localtalk/repository/PostSaveRepository.kt
@@ -3,14 +3,33 @@ package com.meezzi.localtalk.repository
 import android.net.Uri
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 import com.google.firebase.storage.storage
+import com.meezzi.localtalk.data.Post
 
 class PostSaveRepository {
 
+    private val db = Firebase.firestore
     private val storageRef = Firebase.storage.reference
 
     private val currentUser
         get() = FirebaseAuth.getInstance().currentUser
+
+    fun savePostWithImages(
+        post: Post,
+        imageUris: List<Uri>?,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit,
+    ) {
+        val postId = db.collection("posts").document().id
+
+        if (!imageUris.isNullOrEmpty()) {
+            imageUris.forEach { uri ->
+                uploadImage(uri, postId)
+            }
+        }
+        savePost(postId, post, onSuccess, onFailure)
+    }
 
     private fun uploadImage(
         uri: Uri,
@@ -27,5 +46,26 @@ class PostSaveRepository {
         }.addOnSuccessListener { taskSnapshot ->
 
         }
+    }
+
+    private fun savePost(
+        postId: String,
+        post: Post,
+        onSuccess: () -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+
+        val postToSave = post.copy(
+            postId = postId,
+            authorId = currentUser?.uid
+        )
+
+        db.collection("posts")
+            .document(post.city)
+            .collection(post.category.id)
+            .document(post.title)
+            .set(postToSave)
+            .addOnSuccessListener { onSuccess() }
+            .addOnFailureListener { e -> onFailure(e) }
     }
 }
