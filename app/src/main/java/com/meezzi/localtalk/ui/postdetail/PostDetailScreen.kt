98b8b68ab30cc3e5b4d0cc1516d4210243a8204e
Meezzi <alswl-3016@naver.com>
2024-10-17 22:51:31 +0900

    feat: 오류 및 로딩 상태 처리 추가

--- app/src/main/java/com/meezzi/localtalk/ui/postdetail/PostDetailScreen.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/postdetail/PostDetailScreen.kt
@@ -1,9 +1,14 @@
 package com.meezzi.localtalk.ui.postdetail
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.meezzi.localtalk.R
 import com.meezzi.localtalk.ui.common.NavigationTopAppBar
 
 @Composable
@@ -15,6 +20,7 @@ fun PostDetailScreen(
     onNavigateBack: () -> Unit
 ) {
     val post by postDetailViewModel.post.collectAsState()
+    val errorMessage by postDetailViewModel.errorMessage.collectAsState()
 
     Scaffold(
         topBar = {
@@ -24,6 +30,22 @@ fun PostDetailScreen(
             )
         }
     ) { paddingValues ->
+        Box(modifier = Modifier.padding(paddingValues)) {
+            when {
+                errorMessage != null -> {
+                    ErrorView(
+                        message = errorMessage ?: stringResource(R.string.error_message_not_found)
+                    )
+                }
 
+                post == null -> {
+                    LoadingView()
+                }
+
+                else -> {
+                    PostContentView()
+                }
+            }
+        }
     }
 }
