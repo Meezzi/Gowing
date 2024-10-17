9551426929391e5f17bca66865f929ae1a26bbb9
Meezzi <alswl-3016@naver.com>
2024-10-17 22:48:28 +0900

    feat: 게시물 상세 화면 TopAppBar 추가

--- app/src/main/java/com/meezzi/localtalk/ui/postdetail/PostDetailScreen.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/postdetail/PostDetailScreen.kt
@@ -1,8 +1,29 @@
 package com.meezzi.localtalk.ui.postdetail
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.meezzi.localtalk.ui.common.NavigationTopAppBar
 
 @Composable
-fun PostDetailScreen() {
+fun PostDetailScreen(
+    city: String,
+    category: String,
+    postId: String,
+    postDetailViewModel: PostDetailViewModel,
+    onNavigateBack: () -> Unit
+) {
+    val post by postDetailViewModel.post.collectAsState()
 
+    Scaffold(
+        topBar = {
+            NavigationTopAppBar(
+                title = post?.category?.name,
+                onNavigateBack = onNavigateBack
+            )
+        }
+    ) { paddingValues ->
+
+    }
 }
