2af076781817daa97b4c64c6bf39cdd88dc4aa5a
Meezzi <alswl-3016@naver.com>
2024-10-17 13:23:23 +0900

    feat: ViewModel에서 게시물 로드 및 에러 처리 구현

--- app/src/main/java/com/meezzi/localtalk/ui/postdetail/PostDetailViewModel.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/postdetail/PostDetailViewModel.kt
@@ -1,11 +1,43 @@
 package com.meezzi.localtalk.ui.postdetail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.meezzi.localtalk.data.Post
 import com.meezzi.localtalk.repository.PostSaveRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class PostDetailViewModel(private val postSaveRepository: PostSaveRepository) : ViewModel() {
+
+    private val _post = MutableStateFlow<Post?>(null)
+    val post: StateFlow<Post?> = _post
+
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    val errorMessage: StateFlow<String?> = _errorMessage
+
+    fun loadPost(
+        postId: String,
+        city: String,
+        category: String,
+    ) {
+        viewModelScope.launch {
+            postSaveRepository.getPostById(
+                city = city,
+                category = category,
+                postId = postId,
+                onSuccess = { post ->
+                    _post.value = post
+                },
+                onFailure = { exception ->
+                    _post.value = null
+                    _errorMessage.value = exception.message
+                }
+            )
+        }
+    }
 
     companion object {
         fun provideFactory(repository: PostSaveRepository) = viewModelFactory {
