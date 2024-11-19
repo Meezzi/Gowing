647d81dfa0552af3b9c3646f68005c65335f1cb7
Meezzi <alswl-3016@naver.com>
2024-11-19 22:07:45 +0900

    feat: 게시판 리스트 구현

--- app/src/main/java/com/meezzi/localtalk/ui/board/BoardScreen.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/board/BoardScreen.kt
@@ -2,16 +2,21 @@ package com.meezzi.localtalk.ui.board
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +24,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
@@ -61,8 +67,61 @@ fun BoardTopAppBar(
 }
 
 @Composable
-fun BoardContentScreen(innerPadding: PaddingValues) {
+fun BoardContentScreen(
+    innerPadding: PaddingValues,
+    onNavigateToPostItem: (String) -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .padding(12.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        BoardItem(
+            icon = painterResource(R.drawable.ic_pencil),
+            text = stringResource(R.string.myPosts),
+            onClick = {}
+        )
 
+        BoardItem(
+            icon = painterResource(R.drawable.ic_speech_bubble),
+            text = stringResource(R.string.myComments),
+            onClick = {}
+        )
+
+        BoardItem(
+            icon = painterResource(R.drawable.ic_heart),
+            text = stringResource(R.string.likes),
+            onClick = {}
+        )
+
+        BoardItem(
+            icon = painterResource(R.drawable.ic_fire),
+            text = stringResource(R.string.home_hot_posts),
+            onClick = {}
+        )
+
+        BoardItem(
+            icon = painterResource(R.drawable.ic_light),
+            text = stringResource(R.string.home_latest_posts),
+            onClick = {}
+        )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        HorizontalDivider()
+
+        Spacer(modifier = Modifier.height(4.dp))
+
+        Categories.entries.forEach { category ->
+            BoardItem(
+                text = category.displayName,
+                onClick = {
+                }
+            )
+        }
+    }
 }
 
 @Composable
