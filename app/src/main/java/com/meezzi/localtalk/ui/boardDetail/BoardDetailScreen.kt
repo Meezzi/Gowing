894d15386172a40d4f804a2e8a4f17384ccc031f
Meezzi <alswl-3016@naver.com>
2024-11-21 00:39:46 +0900

    feat: 게시물 리스트 구현

--- app/src/main/java/com/meezzi/localtalk/ui/boardDetail/BoardDetailScreen.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/boardDetail/BoardDetailScreen.kt
@@ -1,10 +1,14 @@
 package com.meezzi.localtalk.ui.boardDetail
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import com.meezzi.localtalk.data.Categories
+import com.meezzi.localtalk.data.Post
 import com.meezzi.localtalk.ui.common.NavigationTopAppBar
+import com.meezzi.localtalk.ui.home.screens.PostListSection
 
 @Composable
 fun BoardDetailScreen(
@@ -29,4 +33,21 @@ fun BoardDetailScreen(
 @Composable
 fun BoardDetailContentScreen(innerPaddingValues: PaddingValues) {
 
+}
+
+@Composable
+fun PostListView(
+    postList: List<Post>,
+    onNavigateToPostDetail: (String, String, String) -> Unit
+) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        item {
+            PostListSection(
+                postList = postList,
+                onNavigateToPostDetail = onNavigateToPostDetail
+            )
+        }
+    }
 }
