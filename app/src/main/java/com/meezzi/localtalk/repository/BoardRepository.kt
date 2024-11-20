dc88c3a1e3d3e814588752a03af8ac65decd76c9
Meezzi <alswl-3016@naver.com>
2024-11-20 17:58:47 +0900

    feat: Repository에서 카테고리 별 게시물을 가져오는 기능 구현

--- app/src/main/java/com/meezzi/localtalk/repository/BoardRepository.kt
+++ app/src/main/java/com/meezzi/localtalk/repository/BoardRepository.kt
@@ -1,5 +1,30 @@
 package com.meezzi.localtalk.repository
 
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObjects
+import com.meezzi.localtalk.data.Post
+
 class BoardRepository {
 
+    private val db = Firebase.firestore
+
+    fun fetchPostsByCategory(
+        city: String,
+        category: String,
+        onSuccess: (List<Post>) -> Unit,
+        onFailure: (Exception) -> Unit,
+    ) {
+        val docRef = db.collection("posts/${city}/${category}")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    val topPosts = document.toObjects<Post>()
+                    onSuccess(topPosts)
+                }
+            }
+            .addOnFailureListener { exception ->
+                onFailure(exception)
+            }
+    }
 }
