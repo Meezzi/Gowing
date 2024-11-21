051ed00421db267d43e1364add06d210616f7eed
Meezzi <alswl-3016@naver.com>
2024-11-21 13:42:05 +0900

    feat: 상태에 따른 로딩 화면, 오류 화면 분리

--- app/src/main/java/com/meezzi/localtalk/ui/boardDetail/BoardDetailScreen.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/boardDetail/BoardDetailScreen.kt
@@ -1,22 +1,40 @@
 package com.meezzi.localtalk.ui.boardDetail
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.meezzi.localtalk.data.Categories
 import com.meezzi.localtalk.data.Post
+import com.meezzi.localtalk.ui.common.LoadingView
 import com.meezzi.localtalk.ui.common.NavigationTopAppBar
+import com.meezzi.localtalk.ui.home.HomeViewModel
 import com.meezzi.localtalk.ui.home.screens.PostListSection
+import com.meezzi.localtalk.ui.postdetail.ErrorView
 
 @Composable
 fun BoardDetailScreen(
     categoryId: String?,
+    homeViewModel: HomeViewModel,
+    boardDetailViewModel: BoardDetailViewModel,
     onNavigateBack: () -> Unit,
+    onNavigateToPostDetail: (String, String, String) -> Unit,
 ) {
 
     val categoryName = Categories.getDisplayNameById(categoryId?.uppercase() ?: "") ?: "로딩 중"
+
+    val city by homeViewModel.address.collectAsState()
+    val postList by boardDetailViewModel.postList.collectAsState()
+    val isLoading by boardDetailViewModel.isLoading.collectAsState()
+    val errorMessage by boardDetailViewModel.errorMessage.collectAsState()
 
     Scaffold(
         topBar = {
@@ -26,13 +44,42 @@ fun BoardDetailScreen(
             )
         },
     ) { innerPadding ->
-        BoardDetailContentScreen(innerPadding)
+        BoardDetailContentScreen(
+            innerPadding,
+            isLoading,
+            postList,
+            errorMessage,
+            onNavigateToPostDetail,
+        )
     }
 }
 
 @Composable
-fun BoardDetailContentScreen(innerPaddingValues: PaddingValues) {
+fun BoardDetailContentScreen(
+    innerPadding: PaddingValues,
+    isLoading: Boolean,
+    postList: List<Post>,
+    errorMessage: String,
+    onNavigateToPostDetail: (String, String, String) -> Unit
+) {
+    Column(modifier = Modifier.padding(innerPadding)) {
+        when {
+            errorMessage.isNotEmpty() -> {
+                ErrorView(message = errorMessage)
+            }
 
+            isLoading -> {
+                LoadingView()
+            }
+
+            else -> {
+                PostListView(
+                    postList = postList,
+                    onNavigateToPostDetail = onNavigateToPostDetail
+                )
+            }
+        }
+    }
 }
 
 @Composable
