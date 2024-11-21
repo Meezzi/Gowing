1313957a3a57135b75f0885454d1c23c84624237
Meezzi <alswl-3016@naver.com>
2024-11-21 16:08:59 +0900

    feat: 현재 로그인된 uid와 게시물을 비교하여 내가 작성한 글을 가져오는 기능 구현

--- app/src/main/java/com/meezzi/localtalk/repository/BoardRepository.kt
+++ app/src/main/java/com/meezzi/localtalk/repository/BoardRepository.kt
@@ -1,13 +1,19 @@
 package com.meezzi.localtalk.repository
 
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.firestore
 import com.google.firebase.firestore.toObjects
+import com.meezzi.localtalk.data.Categories
 import com.meezzi.localtalk.data.Post
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.tasks.await
 
 class BoardRepository {
 
     private val db = Firebase.firestore
+    private val currentUser
+        get() = FirebaseAuth.getInstance().currentUser
 
     fun fetchPostsByCategory(
         city: String,
@@ -26,5 +32,27 @@ class BoardRepository {
             .addOnFailureListener { exception ->
                 onFailure(exception)
             }
+    }
+
+    suspend fun fetchMyPosts(
+        onSuccess: (List<Post>) -> Unit,
+        onFailure: (Exception) -> Unit
+    ) {
+        val categoryCollections = Categories.entries.map { it.name.lowercase() }
+
+        try {
+            val allPosts = coroutineScope {
+                categoryCollections.map { collectionName ->
+                    db.collectionGroup(collectionName)
+                        .whereEqualTo("authorId", currentUser?.uid)
+                        .get()
+                        .await()
+                        .toObjects<Post>()
+                }.flatten()
+            }
+            onSuccess(allPosts)
+        } catch (exception: Exception) {
+            onFailure(exception)
+        }
     }
 }
