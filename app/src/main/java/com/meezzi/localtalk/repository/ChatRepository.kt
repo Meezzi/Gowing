e0c57eaaf0c1093c59649b6776d61a1d755da0a5
Meezzi <alswl-3016@naver.com>
2024-12-05 17:33:01 +0900

    feat: 채팅방 생성 및 조회 기능 구현

--- app/src/main/java/com/meezzi/localtalk/repository/ChatRepository.kt
+++ app/src/main/java/com/meezzi/localtalk/repository/ChatRepository.kt
@@ -1,5 +1,38 @@
 package com.meezzi.localtalk.repository
 
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import com.meezzi.localtalk.data.ChatRoom
+
 class ChatRepository {
 
+    private val db = FirebaseFirestore.getInstance()
+    private val currentUserId = Firebase.auth.currentUser?.uid
+
+    fun getOrCreateChatRoom(
+        authorId: String,
+        onResult: (String) -> Unit,
+    ) {
+        val chatRoomsCollection = db.collection("chat_rooms")
+        val participants = listOf(currentUserId.toString(), authorId).sorted()
+
+        chatRoomsCollection
+            .whereEqualTo("participants", participants)
+            .get()
+            .addOnSuccessListener { documents ->
+                if (documents.isEmpty) {
+                    val chatRoomId = chatRoomsCollection.document().id
+                    val newChatRoom = ChatRoom(chatRoomId = chatRoomId, participants = participants)
+
+                    chatRoomsCollection
+                        .document(chatRoomId)
+                        .set(newChatRoom)
+                        .addOnSuccessListener { onResult(chatRoomId) }
+                } else {
+                    val chatRoomId = documents.first().id
+                    onResult(chatRoomId)
+                }
+            }
+    }
 }
