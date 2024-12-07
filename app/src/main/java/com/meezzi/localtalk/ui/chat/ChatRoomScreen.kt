7a2b3999dcd98d2369baab9ddc6029f9ff40a623
Meezzi <alswl-3016@naver.com>
2024-12-08 00:10:31 +0900

    feat: 채팅방 top app bar 구현

--- app/src/main/java/com/meezzi/localtalk/ui/chat/ChatRoomScreen.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/chat/ChatRoomScreen.kt
@@ -1,10 +1,40 @@
 package com.meezzi.localtalk.ui.chat
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.res.stringResource
+import com.meezzi.localtalk.R
+import com.meezzi.localtalk.ui.common.NavigationMenuTopAppBar
 
 @Composable
 fun ChatRoomScreen(
     chatViewModel: ChatViewModel,
+) {
+
+    val userNickname by chatViewModel.userNickname.collectAsState()
+
+    Scaffold(
+        topBar = {
+            NavigationMenuTopAppBar(
+                title = userNickname,
+                menuItems = listOf(stringResource(id = R.string.chat_room_exit)),
+                onMenuItemClick = { },
+                onNavigateBack = { },
+            )
+        },
+    ) { innerPadding ->
+        ChatRoomContentScreen(
+            innerPadding,
+        )
+    }
+}
+
+@Composable
+fun ChatRoomContentScreen(
+    innerPadding: PaddingValues,
 ) {
 
 }
