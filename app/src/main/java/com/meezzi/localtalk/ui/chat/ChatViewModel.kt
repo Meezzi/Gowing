4b6ba4e4aea672b7aa30a095a6a05fa3065f9b79
Meezzi <alswl-3016@naver.com>
2024-12-07 17:28:41 +0900

    feat: 현재 사용자 ID 가져오기

--- app/src/main/java/com/meezzi/localtalk/ui/chat/ChatViewModel.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/chat/ChatViewModel.kt
@@ -4,8 +4,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.meezzi.localtalk.repository.ChatRepository
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class ChatViewModel(private val chatRepository: ChatRepository) : ViewModel() {
+
+    private val _currentUserId = MutableStateFlow("")
+    val currentUserId = _currentUserId
+
+    init {
+        setCurrentUserId()
+    }
+
+    private fun setCurrentUserId() {
+        _currentUserId.value = chatRepository.getCurrentUserId()
+    }
 
     companion object {
         fun provideFactory(repository: ChatRepository) = viewModelFactory {
