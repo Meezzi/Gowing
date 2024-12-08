2c902b7ae0093ebf849ddfe9b5defd1c9f85c3b6
Meezzi <alswl-3016@naver.com>
2024-12-08 15:51:33 +0900

    feat: 메세지 아이템 구현

--- app/src/main/java/com/meezzi/localtalk/ui/chat/ChatRoomScreen.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/chat/ChatRoomScreen.kt
@@ -1,9 +1,14 @@
 package com.meezzi.localtalk.ui.chat
 
+import android.net.Uri
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
@@ -17,15 +22,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.meezzi.localtalk.R
+import com.meezzi.localtalk.data.Message
 import com.meezzi.localtalk.ui.common.NavigationMenuTopAppBar
+import com.meezzi.localtalk.util.TimeFormat
+import kotlinx.coroutines.launch
 
 @Composable
 fun ChatRoomScreen(
@@ -117,4 +128,91 @@ private fun ChatSubmitButton(
             tint = MaterialTheme.colorScheme.primary
         )
     }
+}
+
+@Composable
+fun MessageItem(
+    userNickname: String,
+    userProfileImage: Uri?,
+    message: Message,
+    currentUserId: String,
+) {
+    val isCurrentUser = message.senderId == currentUserId
+
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 4.dp, bottom = 4.dp),
+        horizontalArrangement = if (isCurrentUser) Arrangement.End else Arrangement.Start,
+        verticalAlignment = if (isCurrentUser) Alignment.Bottom else Alignment.Top,
+    ) {
+        if (isCurrentUser) {
+            Time(message)
+            MessageContent(message, isCurrentUser, userNickname)
+        } else {
+            UserProfileImage(userProfileImage)
+            Spacer(modifier = Modifier.width(8.dp))
+            MessageContent(message, isCurrentUser, userNickname)
+            Box(modifier = Modifier.align(Alignment.Bottom)) {
+                Time(message)
+            }
+        }
+    }
+}
+
+@Composable
+fun UserProfileImage(userProfileImage: Uri?) {
+    AsyncImage(
+        model = userProfileImage ?: R.drawable.ic_user,
+        contentDescription = stringResource(id = R.string.profile_image),
+        modifier = Modifier
+            .size(48.dp)
+            .clip(RoundedCornerShape(16.dp)),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+fun MessageContent(
+    message: Message,
+    isCurrentUser: Boolean,
+    userNickname: String,
+) {
+    Column {
+        if (!isCurrentUser) {
+            Text(
+                text = userNickname,
+                fontSize = 13.sp,
+                modifier = Modifier.padding(bottom = 5.dp, end = 5.dp)
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .background(
+                    color = if (isCurrentUser) Color.Magenta.copy(alpha = 0.3f) else Color.LightGray,
+                    shape = if (isCurrentUser)
+                        RoundedCornerShape(16.dp, 16.dp, 16.dp, 16.dp)
+                    else
+                        RoundedCornerShape(0.dp, 16.dp, 16.dp, 16.dp)
+                )
+                .padding(top = 8.dp, bottom = 8.dp, start = 16.dp, end = 16.dp)
+        ) {
+            Text(
+                text = message.content,
+            )
+        }
+    }
+}
+
+@Composable
+private fun Time(message: Message) {
+    val time = TimeFormat().formatMessageTime(message.timestamp)
+
+    Text(
+        text = time,
+        color = Color.Gray,
+        modifier = Modifier.padding(start = 7.dp, end = 7.dp),
+        style = MaterialTheme.typography.bodySmall
+    )
 }
