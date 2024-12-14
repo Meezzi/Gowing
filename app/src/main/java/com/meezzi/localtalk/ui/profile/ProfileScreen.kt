bedd0d6380fe9b3d8d638e730edd3f014c203313
Meezzi <alswl-3016@naver.com>
2024-12-14 23:26:33 +0900

    feat: 프로필 화면 UI 변경

--- app/src/main/java/com/meezzi/localtalk/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/profile/ProfileScreen.kt
@@ -18,76 +18,69 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
 import com.meezzi.localtalk.R
+import com.meezzi.localtalk.ui.common.CustomTopAppBar
+import com.meezzi.localtalk.ui.common.LoadingView
 
 @Composable
 fun ProfileScreen(
     profileViewModel: ProfileViewModel,
     onEditProfileClick: () -> Unit,
 ) {
-
+    val isLoading by profileViewModel.isLoading.collectAsState()
     val nickname by profileViewModel.nickname.collectAsState()
     val region by profileViewModel.region.collectAsState()
     val profileImageUri by profileViewModel.profileImageUri.collectAsState()
 
+    if (isLoading) {
+        LoadingView()
+    } else {
+        Scaffold(
+            topBar = { CustomTopAppBar(title = "프로필") }
+        ) { innerPadding ->
+            ProfileContentScreen(
+                innerPadding,
+                nickname,
+                region,
+                profileImageUri,
+                onEditProfileClick,
+            )
+        }
+    }
+}
+
+@Composable
+fun ProfileContentScreen(
+    innerPadding: PaddingValues,
+    nickname: String,
+    region: String,
+    profileImageUri: Uri?,
+    onEditProfileClick: () -> Unit,
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(10.dp)
-            .verticalScroll(rememberScrollState()),
+            .padding(innerPadding)
     ) {
-        Spacer(modifier = Modifier.padding(25.dp))
-
         ProfileHeader(
-            nickname,
-            region,
-            profileImageUri,
-            onEditProfileClick = onEditProfileClick
+            name = nickname,
+            region = region,
+            profileImage = profileImageUri,
+            onEditProfileClick = onEditProfileClick,
         )
-
-        Spacer(modifier = Modifier.padding(10.dp))
-
-        PostOptionRow()
-    }
-}
-
-@Composable
-fun PostOptionRow() {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        CustomButton(text = stringResource(R.string.written_post), onClick = { })
-        CustomButton(text = stringResource(R.string.recommended_post), onClick = { })
-    }
-}
-
-@Composable
-fun CustomButton(
-    text: String,
-    onClick: () -> Unit
-) {
-    Button(
-        onClick = onClick,
-        border = BorderStroke(0.5.dp, Color.LightGray),
-        colors = ButtonDefaults.outlinedButtonColors(
-            contentColor = Color.Black,
-            containerColor = Color.Transparent,
-        ),
-        shape = RectangleShape,
-    ) {
-        Text(text)
     }
 }
 
@@ -106,44 +99,52 @@ fun ProfileHeader(
         horizontalArrangement = Arrangement.SpaceBetween,
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
+            ProfileImage(profileImage)
 
-            val painter = if (profileImage != null) {
-                rememberAsyncImagePainter(model = profileImage)
-            } else {
-                painterResource(id = R.drawable.ic_default_profile)
-            }
-
-            Image(
-                painter = painter,
-                contentDescription = "Profile Image",
-                modifier = Modifier
-                    .size(60.dp)
-                    .clip(CircleShape),
-            )
             Spacer(modifier = Modifier.width(16.dp))
-            Column {
-                Text(
-                    text = name,
-                    fontSize = 25.sp,
-                    fontWeight = FontWeight.Normal,
-                    color = Color.Black,
-                )
-                Spacer(modifier = Modifier.padding(3.dp))
-                Text(
-                    text = "Region",
-                    fontSize = 16.sp,
-                    color = Color.DarkGray,
-                )
-            }
+
+            ProfileNameAndRegion(name, region)
         }
+
         IconButton(
             onClick = { onEditProfileClick() }
         ) {
             Icon(
                 painter = painterResource(id = R.drawable.ic_note_pencil),
-                contentDescription = "Favorite",
-                Modifier.size(30.dp),
+                contentDescription = stringResource(id = R.string.profile_edit),
+                modifier = Modifier.size(30.dp),
             )
         }
+    }
+}
+
+@Composable
+private fun ProfileImage(profileImage: Uri?) {
+    AsyncImage(
+        model = profileImage ?: R.drawable.ic_default_profile,
+        contentDescription = stringResource(
+            id = R.string.profile_image
+        ),
+        modifier = Modifier
+            .size(60.dp)
+            .clip(CircleShape)
+    )
+}
+
+@Composable
+private fun ProfileNameAndRegion(name: String, region: String) {
+    Column {
+        Text(
+            text = name,
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Normal,
+            color = Color.Black,
+        )
+        Spacer(modifier = Modifier.height(3.dp))
+        Text(
+            text = region,
+            fontSize = 16.sp,
+            color = Color.DarkGray,
+        )
     }
 }
