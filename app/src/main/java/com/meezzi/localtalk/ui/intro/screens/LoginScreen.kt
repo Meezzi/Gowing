d3497e29c34051424a3fc26f5f826eac5220107c
Meezzi <alswl-3016@naver.com>
2024-12-17 05:30:37 +0900

    design: 환영 메세지 UI 개선

--- app/src/main/java/com/meezzi/localtalk/ui/intro/screens/LoginScreen.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/intro/screens/LoginScreen.kt
@@ -2,16 +2,17 @@ package com.meezzi.localtalk.ui.intro.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.airbnb.lottie.compose.LottieAnimation
 import com.airbnb.lottie.compose.LottieCompositionSpec
 import com.airbnb.lottie.compose.animateLottieCompositionAsState
@@ -35,6 +36,25 @@ fun LoginScreen(
             SignInGoogleButton(onClick = onSignInClick)
         }
     }
+}
+
+@Composable
+fun WelcomeText() {
+    Text(
+        text = stringResource(R.string.welcome_text),
+        fontWeight = FontWeight.SemiBold,
+        color = Color.White,
+        textAlign = TextAlign.Start,
+        style = MaterialTheme.typography.headlineLarge
+    )
+    Spacer(modifier = Modifier.height(10.dp))
+    Text(
+        text = stringResource(R.string.gowing),
+        fontWeight = FontWeight.Bold,
+        color = Color(0xFF0F2D89),
+        textAlign = TextAlign.Start,
+        style = MaterialTheme.typography.headlineLarge
+    )
 }
 
 @Composable
