828244226b074fa2c0a53b56895bc6f6a8c4c71b
Meezzi <alswl-3016@naver.com>
2024-12-17 00:06:48 +0900

    feat: title에 따른 UI 구현

--- app/src/main/java/com/meezzi/localtalk/ui/setting/SettingInfoScreen.kt
+++ app/src/main/java/com/meezzi/localtalk/ui/setting/SettingInfoScreen.kt
@@ -1,8 +1,22 @@
 package com.meezzi.localtalk.ui.setting
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.meezzi.localtalk.R
 import com.meezzi.localtalk.ui.common.CenterTopAppBar
+import com.meezzi.localtalk.ui.common.CustomAlertDialog
 import com.meezzi.localtalk.ui.profile.ProfileViewModel
 
 @Composable
@@ -26,7 +40,11 @@ fun SettingInfoScreen(
             )
         }
     ) { innerPadding ->
-
+        when (title) {
+            stringResource(id = R.string.my_information) -> {
+                MyInformation(innerPadding, email)
+            }
+        }
     }
 }
 
