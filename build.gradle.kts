a34ecc28443f84e18457dfc05b03d09a7958d98f
Meezzi <alswl-3016@naver.com>
2024-12-26 00:40:18 +0900

    build: hilt 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.google.gms.google.service) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
+    alias(libs.plugins.google.dagger.hilt.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
